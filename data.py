70c9655f8c35910a2aa864b4ee9b23cc3f161d5e
Fredy Goolberth Quispe Neira <fredy.quispe@ucsp.edu.pe>
2024-06-24 14:27:00 -0500

    Apply functions updated in data.py

--- data.py
+++ data.py
@@ -7,7 +7,8 @@ from sklearn.cluster import HDBSCAN, KMeans
 
 class DataManager():
     def __init__(self):
-        pass
+        self.workspace_data = {}
+        self.workspace_labels = {}
     
     def connect_managers(self, conn_manager, ui_manager) -> None:
         self.conn_manager = conn_manager
@@ -24,20 +25,30 @@ class DataManager():
     # y devuelve los puntos 2D en dos listas con el componente X y Y respectivamente
 
     def apply_umap(self, workspace_id: int, n_neighbors: int, min_dist: float, metric: str) -> tuple[list[float], list[float]]:
-        return np.random.rand(100).tolist(), np.random.rand(100).tolist()
+        umap = UMAP(n_components=3, n_neighbors=n_neighbors, min_dist=min_dist, metric=metric)
+        self.workspace_data[workspace_id] = umap.fit_transform(np.random.rand(100, 1000))
+        return self.workspace_data[workspace_id][:, 0].tolist(), self.workspace_data[workspace_id][:, 1].tolist()
     
     def apply_tsne(self, workspace_id: int, learning_rate: float, perplexity: float, early_exaggeration: float, metric: str) -> tuple[list[float], list[float]]:
-        return np.random.rand(100).tolist(), np.random.rand(100).tolist()
+        tsne = TSNE(n_components=3, learning_rate=learning_rate, perplexity=perplexity, early_exaggeration=early_exaggeration, metric=metric)
+        self.workspace_data[workspace_id] = tsne.fit_transform(np.random.rand(100, 1000))
+        return self.workspace_data[workspace_id][:, 0].tolist(), self.workspace_data[workspace_id][:, 1].tolist()
     
     def apply_pca(self, workspace_id: int, whiten: bool, tolerance: float, svd_solver: str) -> tuple[list[float], list[float]]:
-        return np.random.rand(100).tolist(), np.random.rand(100).tolist()
-    
+        pca = PCA(n_components=3, whiten=whiten, tolerance=tolerance, svd_solver=svd_solver)
+        self.workspace_data[workspace_id] = pca.fit_transform(np.random.rand(100, 1000)) 
+        return self.workspace_data[workspace_id][:, 0].tolist(), self.workspace_data[workspace_id][:, 1].tolist()
+    # TODO: Conectar datos de imagenes preprocesadas con los apply_xxxx()
     ### CLUSTERING ###
     # Aplica <METODO> a los componentes X y Y, envia los labels al navegador
     # y devuelve los mismos labels
 
     def apply_hdbscan(self, workspace_id: int, xdata: list[float], ydata: list[float], min_cluster_size: int, min_samples: int, cluster_selection_epsilon: float, cluster_selection_method: str) -> list[int]:
-        return []
+        hdbscan = HDBSCAN(min_cluster_size=min_cluster_size, min_samples=min_samples, cluster_selection_epsilon=cluster_selection_epsilon, cluster_selection_method=cluster_selection_method)
+        self.workspace_data[workspace_id] = hdbscan.fit_predict(self.workspace_data[workspace_id])
+        return self.workspace_data[workspace_id].tolist()
 
     def apply_kmeans(self, workspace_id: int, xdata: list[float], ydata: list[float], n_clusters: int, max_iter: int, init: str, algorithm: str) -> list[int]:
-        return []
+        kmeans = KMeans(n_clusters=n_clusters, max_iter=max_iter, init=init, algorithm=algorithm)
+        self.workspace_labels[workspace_id] = kmeans.fit_predict(self.workspace_data[workspace_id])
+        return self.workspace_labels[workspace_id].tolist()
